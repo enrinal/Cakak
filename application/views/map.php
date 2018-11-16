43396676cc182e76e2ea7acc2022c0d15d9a36c7
Nanda Meilly Annisya <nandameilly@gmail.com>
2018-11-16 09:04:04 +0700

    remove body for API add by Backend Dev

--- application/views/map.php
+++ application/views/map.php
@@ -25,18 +25,5 @@
         <link rel="icon" href="<?=base_url().'assets/images/LOGO.png'?>" type="image/gif">
 
   </head>
-  <body>
-    <div id="map"></div>
-    <script>
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -5.357310, lng: 105.314392},
-          zoom: 14
-        });
-      }
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCSLG6GQ4GgWrBYM66skOvCsrTS0Ma9jHI&callback=initMap"
-    async defer></script>
-  </body>
+  
 </html>
